ab622acabd5f92602d3153c0f63ddf8b87721db1
Florian Polster <couchpolster@icqmail.com>
2019-07-07 11:22:57 +0200

    Update aliases

--- .aliases.sh
+++ .aliases.sh
@@ -3,10 +3,10 @@ alias ll='ls -lAhF --group-directories-first'
 [ -f /usr/bin/exa ] && alias ls="exa --git --group-directories-first" && alias ll="ls -l --header" # Replace ls with exa
 [ -f /usr/bin/bat ] && alias cat="bat --tabs=4" # Replace cat with https://github.com/sharkdp/bat
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-alias www='python -m SimpleHTTPServer 8000' # start a web server in any folder you'd like
+alias www='python3 -m http.server 80' # start a web server in any folder you'd like
 alias ipe='curl ipinfo.io/ip' # display external IP
 alias ipi='ipconfig getifaddr en0' # display internal IP
-alias getpass="openssl rand -base64 12" # generate random 12 character password
+alias batt="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage"
 
 alias g='git'
 alias ga='git add'
@@ -22,3 +22,4 @@ alias weath='curl wttr.in'
 # !$ argument of the last command
 # !^ first argument of the last command
 # !keyword skip Ctrl+R and run last command that contained keyword
+
